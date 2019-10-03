5d9cfaa9c1cea4f5e836b8fdc0c3d0ef96e1452f
Eddie Sholl <esholl@zendesk.com>
2019-10-03 21:22:58 +1000

    Adding TodoCreator but not hooked up yet

---
+++ pages/TodoCreator.js
@@ -0,0 +1,8 @@
+export const TodoCreator = () => {
+  return (
+    <div>
+      <input></input>
+      <button>Add me</button>
+    </div>
+  )
+}
--- pages/TodoManager.js
+++ pages/TodoManager.js
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 
 import { TodoList } from "./TodoList"
+import { TodoCreator } from './TodoCreator'
 
 export const TodoManager = () => {
   const [todos, setTodos] = useState(['Remember the milk', 'Something else'])
 
   return (
-    <TodoList todos={todos} />
+    <div>
+      <TodoList todos={todos} />
+      <TodoCreator />
+    </div>
   )
 }
