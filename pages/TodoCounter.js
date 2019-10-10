22846cd1e2398130401724055106488801bd0492
Eddie Sholl <esholl@zendesk.com>
2019-10-10 21:33:22 +1100

    Adding example with prop-types validation

--- pages/TodoCounter.js
+++ pages/TodoCounter.js
@@ -1,7 +1,13 @@
+import PropTypes from 'prop-types'
+
 export const TodoCounter = ({ todos }) => {
   return (
     <div>
       There are currently {todos.length} things to remember
     </div>
   )
+}
+
+TodoCounter.propTypes = {
+  todos: PropTypes.array.isRequired
 }
